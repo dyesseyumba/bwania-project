1c91ef72b3dcd2f3895f9be42349eabd538d81c0
YUMBA Nduba Dyesse <yumba.nduba@gmail.com>
2015-02-27 01:09:26 +0100

    (+) Initial Commit.

---
+++ src/backend/Core/bwania-project.Core.Business/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bwania_project.Core.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/backend/Infrastructure/bwania-project.Infrastructure.Business/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bwania_project.Infrastructure.Business
+{
+    public class Class1
+    {
+    }
+}
