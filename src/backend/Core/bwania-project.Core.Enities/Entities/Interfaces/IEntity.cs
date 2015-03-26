7f5511414ebc19de457a097bf3a632ee875ed522
Djesse Yumba Nduba <yumba.nduba@gmail.com>
2015-03-26 22:41:48 +0100

    (+) Added Some Exceptions extentions methods on Data Access layer.
    (*) Fixed some spelling.

---
+++ src/backend/Core/bwania-project.Core.Enities/Entities/Interfaces/IEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace bwaniaProject.Core.Entities
+{
+    public interface IEntity
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cas.
+        /// </summary>
+        /// <value>
+        /// The cas.
+        /// </value>
+        ulong Cas { get; set; }
+    }
+}
---
+++ src/backend/Infrastructure/bwania-project.Infrastructure.Data/Extensions/EntityExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using bwaniaProject.Core.Entities;
+using Couchbase;
+
+namespace bwaniaProject.Infrastructure.Data.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static IDocument<T> Wrap<T>(this T entity) where T : IEntity
+        {
+            return new Document<T>
+            {
+                Id = entity.Id,
+                Cas = entity.Cas,
+                Content = entity
+            };
+        }
+
+        public static T UnWrap<T>(this IDocument<T> document) where T : IEntity
+        {
+            var entity = document.Content;
+            entity.Cas = document.Cas;
+            entity.Id = document.Id;
+            return entity;
+        }
+    }
+}
