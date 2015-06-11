dce1968c2eacaadd67819ad8b7d75e3a73e4dcf9
Djesse Yumba Nduba <yumba.nduba@gmail.com>
2015-06-11 19:07:10 +0100

    (+) Added ElasticSearch Model.

---
+++ src/backend/Core/bwania-project.Core.Enities/ElasticSearchIndex.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ElasticSearchIndex.cs" company="Bwania development team">
+//    Copyright (c) 2014 - 2015 Bwania development team. All rights reserved.
+//  </copyright>  
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BwaniaProject.Entities
+{
+    public class ElasticSearchIndex : IElasticSearchIndex
+    {
+        public Meta Meta { get; set; }
+    }
+
+    public class Meta : IMeta
+    {
+        public string Id { get; set; }
+
+        public string Rev { get; set; }
+
+        public int Flags { get; set; }
+
+        public int Expiration { get; set; }
+    }
+}
